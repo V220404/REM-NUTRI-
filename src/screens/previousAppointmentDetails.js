bad9809bacfdbe6676d2136ea4677ca71367d2f2
V220404 <sankhevarun22@gmail.com>
2024-07-12 18:49:03 +0530

    appointment detail screen

---
+++ src/screens/previousAppointmentDetails.js
@@ -0,0 +1,16 @@
+// import { View, Text, SafeAreaView } from 'react-native'
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <SafeAreaView>
+//     <View>
+
+
+      
+//     </View>
+//     </SafeAreaView>
+//   )
+// }
+
+// export default App
